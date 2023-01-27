10551db46cef4272733b7d55fe4bfc04e7dc35b8
Torbjørn Øvrebekk <torbjorn.ovrebekk@nordicsemi.no>
2023-01-27 12:48:04 +0100

    sx1509b sample integrated in app_led.c to support PWM control of LED's

--- include/app_led.h
+++ include/app_led.h
@@ -1,10 +1,25 @@
 #ifndef __APP_LED_H
 #define __APP_LED_H
 
-typedef enum {LED_COLOR_BLACK, LED_COLOR_RED} app_led_color_t;
+#include <zephyr/kernel.h>
+
+#define LED_COLOR_BLACK 	0x000000
+#define LED_COLOR_RED		0x0000FF
+#define LED_COLOR_YELLOW	0x00FFFF
+#define LED_COLOR_GREEN		0x00FF00
+#define LED_COLOR_BLUE		0xFF0000
+#define LED_COLOR_PURPLE	0xFF00FF
+#define LED_COLOR_TEAL		0xFFFF00
+#define LED_COLOR_WHITE		0xFFFFFF
+
+#define COLOR_CH_RED(a) ((a) & 0xFF)
+#define COLOR_CH_GREEN(a) (((a) >> 8) & 0xFF)
+#define COLOR_CH_BLUE(a) (((a) >> 16) & 0xFF)
+
+typedef uint32_t led_color_t;
 
 int app_led_init(void);
 
-int app_led_toggle(app_led_color_t color1);
+int app_led_toggle(led_color_t color1);
 
 #endif
--- src/app_led.c
+++ src/app_led.c
@@ -1,29 +1,56 @@
 #include <app_led.h>
-#include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/gpio/gpio_sx1509b.h>
 #include <zephyr/device.h>
 
-#define LED0_NODE DT_ALIAS(led0)
+#define NUMBER_OF_LEDS 3
+#define RED_LED DT_GPIO_PIN(DT_NODELABEL(led0), gpios)
+#define GREEN_LED DT_GPIO_PIN(DT_NODELABEL(led1), gpios)
+#define BLUE_LED DT_GPIO_PIN(DT_NODELABEL(led2), gpios)
 
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+const struct device *dev_sx1509b;
+
+static const gpio_pin_t rgb_pins[] = {
+	RED_LED,
+	GREEN_LED,
+	BLUE_LED,
+};
 
 int app_led_init(void)
 {
 	int ret;
 	
-	if (!device_is_ready(led.port)) {
+	dev_sx1509b = DEVICE_DT_GET(DT_NODELABEL(sx1509b));
+
+	if (!device_is_ready(dev_sx1509b)) {
+		printk("sx1509b: device not ready.\n");
 		return -1;
 	}
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
-		return -1;
+	for (int i = 0; i < NUMBER_OF_LEDS; i++) {
+		ret = sx1509b_led_intensity_pin_configure(dev_sx1509b, rgb_pins[i]);
+
+		if (ret < 0) {
+			printk("Error configuring pin for LED intensity\n");
+			return ret;
+		}
 	}
 
 	return 0;
 }
 
-int app_led_toggle(app_led_color_t color1)
+int app_led_toggle(led_color_t color1)
 {
-	return gpio_pin_toggle_dt(&led);
+	static bool led_on = true;
+	if(led_on) {
+		sx1509b_led_intensity_pin_set(dev_sx1509b, RED_LED, COLOR_CH_RED(color1));
+		sx1509b_led_intensity_pin_set(dev_sx1509b, GREEN_LED, COLOR_CH_GREEN(color1));
+		sx1509b_led_intensity_pin_set(dev_sx1509b, BLUE_LED, COLOR_CH_BLUE(color1));
+	} else {
+		sx1509b_led_intensity_pin_set(dev_sx1509b, RED_LED, 0);
+		sx1509b_led_intensity_pin_set(dev_sx1509b, GREEN_LED, 0);
+		sx1509b_led_intensity_pin_set(dev_sx1509b, BLUE_LED, 0);
+	}
+	led_on = !led_on;
+	return 0;
 }
